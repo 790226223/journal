94d13ce997d89c5034600c16c33557eb40716f55
chenweipan <chenweipan@zccp.com>
2017-06-20 16:09:22 +0800

    修改spring外部依赖的版本

--- src/main/java/chenweipan/journal/repository/JournalUserRepository.java
+++ src/main/java/chenweipan/journal/repository/JournalUserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import chenweipan.journal.po.JournalUser;
 
 public interface JournalUserRepository extends JpaRepository<JournalUser, Long> {
-
+    public JournalUser findByUserName(String username);
 }
